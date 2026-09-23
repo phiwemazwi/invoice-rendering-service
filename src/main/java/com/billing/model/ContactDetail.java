a8cb683d83d16a8092e80dab6b9674250aaf33e2
Phiwe.Mazwi <phiwe.mazwi@eoh.com>
2026-09-23 21:52:17 +0200

    feat(model): Add Java object representing XML structure

---
+++ src/main/java/com/billing/model/ContactDetail.java
@@ -0,0 +1,14 @@
+package com.billing.model;
+
+public class ContactDetail {
+    private String Title;
+    private String Name;
+    private String Surname;
+    private String Gender;
+    private String ContactNumber;
+    private String Email;
+    private String Language;
+
+
+
+}
